143c76a44d0f0d407bd974d54763fef59f717975
Septu <snowkideagle1@gmail.com>
2024-09-12 16:09:15 -0700

    Update script.js

--- mainweb/script.js
+++ mainweb/script.js
@@ -12,4 +12,14 @@ document.addEventListener("DOMContentLoaded", function() {
             const targetPage = this.getAttribute('href');
 
             // Add slide-out effect and activate decoding transition
-            page.classList.remove('
+            page.classList.remove('slide-in');
+            page.classList.add('slide-out');
+            overlay.classList.add('active');
+
+            // After transition, redirect to target page
+            setTimeout(() => {
+                window.location.href = targetPage;
+            }, 1000); // Match with CSS transition time
+        });
+    });
+});
