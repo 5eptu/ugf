21c89037693df2403adac77642df59af00bd9a22
Septu <snowkideagle1@gmail.com>
2024-10-03 13:47:14 -0700

    Delete mainweb directory

--- mainweb/js/gallery-script.js
+++
@@ -1,5 +0,0 @@
-// Horizontal Scrolling Effect
-document.querySelector('.gallery-container').addEventListener('wheel', (evt) => {
-    evt.preventDefault();
-    document.querySelector('.gallery-grid').scrollLeft += evt.deltaY;
-});
